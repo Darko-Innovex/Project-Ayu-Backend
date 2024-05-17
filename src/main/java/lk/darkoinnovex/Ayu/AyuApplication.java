6274ff0bf37678383d8b22a8ca6a400eae1972eb
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-05-17 20:21:34 +0530

    Project initialized

---
+++ src/main/java/lk/darkoinnovex/Ayu/AyuApplication.java
@@ -0,0 +1,13 @@
+package lk.darkoinnovex.Ayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/darkoinnovex/Ayu/AyuApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.darkoinnovex.Ayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
