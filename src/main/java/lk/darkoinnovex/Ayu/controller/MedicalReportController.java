0e5c529acf5ca37da2c68c6b56283d1ff14a5ee5
AshanNaveen <ashannvn@gmail.com>
2024-06-28 12:23:12 +0530

    Medical Report Controller Updated

--- src/main/java/lk/darkoinnovex/Ayu/controller/MedicalReportController.java
+++ src/main/java/lk/darkoinnovex/Ayu/controller/MedicalReportController.java
@@ -1,29 +1,42 @@
 package lk.darkoinnovex.Ayu.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import lk.darkoinnovex.Ayu.dto.MedicalReportDTO;
+import lk.darkoinnovex.Ayu.entity.MedicalReport;
+import lk.darkoinnovex.Ayu.service.MedicalReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import lk.darkoinnovex.Ayu.entity.MedicalReport;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class MedicalReportController {
 
-    //TODO: autowire medical report service layer
-    
+    @Autowired
+    private MedicalReportService medicalReportService;
+
     // Return medical report of a specific appointment
-    @GetMapping("/appointment/{id}/medical_report") 
-    public ResponseEntity<MedicalReport> getMedicalReportOfAppointment(@PathVariable Long id) {
-        return ResponseEntity.status(200).body(new MedicalReport());
+    @GetMapping("/appointment/{id}/medical_report")
+    public ResponseEntity<MedicalReportDTO> getMedicalReportOfAppointment(@PathVariable Long id) {
+        MedicalReportDTO report = medicalReportService.getReportByAppoinmentId(id);
+
+        if (report != null) {
+            return ResponseEntity.status(200).body(report);
+        }
+        return ResponseEntity.status(404).body(null);
     }
 
     // Return all medical reports of a specific patient
-    @GetMapping("/patient/{id}/appointment/medical_report") 
-    public ResponseEntity<List<MedicalReport>> getAllMedicalReportOfPatient(@PathVariable Long id) {
-        return ResponseEntity.status(200).body(new ArrayList<MedicalReport>());
+    @GetMapping("/patient/{id}/appointment/medical_report")
+    public ResponseEntity<List<MedicalReportDTO>> getAllMedicalReportOfPatient(@PathVariable Long id) {
+        List<MedicalReportDTO> reports= medicalReportService.getReportsByPatientId(id);
+        if (reports != null) {
+            return ResponseEntity.status(200).body(reports);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
